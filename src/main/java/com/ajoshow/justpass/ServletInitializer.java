19370d55acce1e992fb2f42d1674c6ad9679bc70
ajoshow <ajoshow@gmail.com>
2015-08-31 02:05:08 +0800

    First Init

---
+++ src/main/java/com/ajoshow/justpass/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ajoshow.justpass;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JustPassApplication.class);
+	}
+
+}
